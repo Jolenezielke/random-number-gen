525c4fa275907122581a7793bfaa0a6ad8900bbd
Jolenezielke <102322445+Jolenezielke@users.noreply.github.com>
2022-04-08 14:49:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+'''
+import random 
+
+generate a random integer and store it in a variable. myNum = random.randint(10,50) input - Ask the user to enter a number.  if/else statements. If the number the user entered equals the random number, print, you win.  if the number is too low, print "Too Low"  if the number is too high, print "Too High"
+'''
